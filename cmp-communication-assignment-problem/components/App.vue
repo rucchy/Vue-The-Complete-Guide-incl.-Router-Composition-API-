07453f211db7e3cb3e66c2a664c832a820d808d9
Jose Ruano <jose.ruano@dehn.de>
2025-04-02 07:44:05 +0200

    feat: add the problem 6

---
+++ cmp-communication-assignment-problem/components/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <active-user
+    v-if="activeUser"
+    :username="activeUser.username"
+    :age="activeUser.age"
+  />
+  <user-data @submit="submit" />
+</template>
+<script>
+export default {
+  data() {
+    return {
+      activeUser: null,
+    };
+  },
+  methods: {
+    submit({ username, age }) {
+      this.activeUser = {
+        username: username,
+        age: age,
+      };
+    },
+  },
+};
+</script>
+
+<style>
+@import url("../public/styles.css");
+</style>
---
+++ cmp-communication-assignment-problem/components/UserData.vue
@@ -0,0 +1,36 @@
+<template>
+  <form @submit.prevent="submit">
+    <div>
+      <label for="username">Username</label>
+      <input type="text" id="username" v-model="username" />
+    </div>
+    <div>
+      <label for="age">Age</label>
+      <input type="number" id="age" v-model="age" />
+    </div>
+    <button type="submit">Submit</button>
+  </form>
+</template>
+<script>
+export default {
+  emits: ["submit"],
+  data() {
+    return {
+      username: "",
+      age: null,
+    };
+  },
+  methods: {
+    submit() {
+      this.$emit("submit", {
+        username: this.username,
+        age: this.age,
+      });
+    },
+  },
+};
+</script>
+
+<style>
+@import url("../public/form.css");
+</style>
